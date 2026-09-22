db485209996e84125fcb8d032bbe7d39d0ffe07b
pranab roy <roypranab198@gmail.com>
2026-09-23 00:02:45 +0530

    Finalize LocalJobHub jobs workflow and UI

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.LOCALJOBHUB_SUPABASE_URL = "https://xmyuxbgkpuskszpuqibn.supabase.co";
+window.LOCALJOBHUB_SUPABASE_KEY = "sb_publishable_MI8J2NT666ZSBqawQdJR4Q_wdqIMfeE";
+window.LocalJobHubSupabase = window.supabase.createClient(
+  window.LOCALJOBHUB_SUPABASE_URL,
+  window.LOCALJOBHUB_SUPABASE_KEY
+);
